0084d9150bacd82619d698a2021d88d1f41c4af9
Fardin Ahsan <fardin2002@cseku.ac.bd>
2026-06-24 11:02:31 +0600

    Parent Service Class Introduced for common CRUD methods

--- server/src/modules/employee/employeeSalary/empSalary.service.ts
+++ server/src/modules/employee/employeeSalary/empSalary.service.ts
@@ -3,49 +3,23 @@ import EmployeeSalaryRepository from './empSalary.repository.js';
 import type EmpSalaryDto from './dtos/empSalary.dto.js';
 import type UpdateEmpSalaryDto from './dtos/updateEmpSalary.dto.js';
 import type CreateEmpSalaryDto from './dtos/createEmpSalary.dto.js';
+import BaseService from '../../../shared/interfaces/baseService.js';
 
-class EmployeeSalaryService {
-  private readonly employeeSalaryRepository: EmployeeSalaryRepository;
-
-  constructor(private readonly logger: ILogger) {
-    this.employeeSalaryRepository = new EmployeeSalaryRepository();
+class EmployeeSalaryService extends BaseService <
+  EmpSalaryDto,
+  CreateEmpSalaryDto,
+  UpdateEmpSalaryDto,
+  number,
+  EmployeeSalaryRepository
+> {
+  constructor(
+    mLogger: ILogger,
+    mRepository: EmployeeSalaryRepository,
+  ) {
+    super(mLogger, mRepository);
   }
 
-  async get(id: number): Promise<EmpSalaryDto | null> {
-    return this.employeeSalaryRepository.get(id);
-  }
 
-  async getAll(): Promise<EmpSalaryDto[]> {
-    return this.employeeSalaryRepository.getAll();
-  }
-
-  async create(pMutable: CreateEmpSalaryDto): Promise<EmpSalaryDto> {
-    return this.employeeSalaryRepository.create(pMutable);
-  }
-
-  async createMany(
-    pMutableList: CreateEmpSalaryDto[],
-  ): Promise<EmpSalaryDto[]> {
-    return this.employeeSalaryRepository.createMany(pMutableList);
-  }
-
-  async update(pMutable: UpdateEmpSalaryDto): Promise<EmpSalaryDto> {
-    return this.employeeSalaryRepository.update(pMutable);
-  }
-
-  async updateMany(
-    pMutableList: UpdateEmpSalaryDto[],
-  ): Promise<EmpSalaryDto[]> {
-    return this.employeeSalaryRepository.updateMany(pMutableList);
-  }
-
-  async delete(id: number): Promise<number> {
-    return this.employeeSalaryRepository.delete(id);
-  }
-
-  async deleteMany(ids: number[]): Promise<number[]> {
-    return this.employeeSalaryRepository.deleteMany(ids);
-  }
 }
 
 export default EmployeeSalaryService;
---
+++ server/src/shared/interfaces/baseService.ts
@@ -0,0 +1,59 @@
+import type { ILogger } from './logger.interface.js';
+import type IRepository from './repository.interface.js';
+
+abstract class BaseService<
+  TEntity,
+  TCreateDto,
+  TUpdateDto,
+  TIdentifier = number,
+  TRepository extends IRepository<
+    TEntity,
+    TCreateDto,
+    TUpdateDto,
+    TIdentifier
+  > = IRepository<
+    TEntity,
+    TCreateDto,
+    TUpdateDto,
+    TIdentifier
+  >,
+> {
+  protected constructor(
+    protected readonly mLogger: ILogger,
+    protected readonly mRepository: TRepository,
+  ) {}
+
+  async get(id: TIdentifier): Promise<TEntity | null> {
+    return this.mRepository.get(id);
+  }
+
+  async getAll(): Promise<TEntity[]> {
+    return this.mRepository.getAll();
+  }
+
+  async create(dto: TCreateDto): Promise<TEntity> {
+    return this.mRepository.create(dto);
+  }
+
+  async createMany(dtos: TCreateDto[]): Promise<TEntity[]> {
+    return this.mRepository.createMany(dtos);
+  }
+
+  async update(dto: TUpdateDto): Promise<TEntity> {
+    return this.mRepository.update(dto);
+  }
+
+  async updateMany(dtos: TUpdateDto[]): Promise<TEntity[]> {
+    return this.mRepository.updateMany(dtos);
+  }
+
+  async delete(id: TIdentifier): Promise<TIdentifier> {
+    return this.mRepository.delete(id);
+  }
+
+  async deleteMany(ids: TIdentifier[]): Promise<TIdentifier[]> {
+    return this.mRepository.deleteMany(ids);
+  }
+}
+
+export default BaseService;
